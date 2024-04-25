38963437903e4af6f6d8f20b0d64dc258e4da170
Glauber <glauber.charles2012@gmail.com>
2024-04-24 21:12:40 -0300

    Primeiro commit do projeto

---
+++ CatdogWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CatdogWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
